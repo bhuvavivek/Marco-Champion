bcd29bfb3ab7e67d051acd9eb913a51b2191b425
bhuvavivek <vivekpate26@gmail.com>
2024-06-19 21:39:56 +0530

    join button fix

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,7 +5,10 @@ const Header = ({ onClick }) => {
       <div className="header" id="header">
         <div className="cout">
           JOIN THE BIGGEST MOVEMENT TO ESCAPE THE MATRIX
-          <button className="header btn" onClick={onClick}>JOIN NOW</button>
+          <button className="header btn " onClick={onClick} style={{
+            background: '#216023 !important',
+            color: 'white !important',
+          }}>JOIN NOW</button>
         </div>
       </div>
     </div>
